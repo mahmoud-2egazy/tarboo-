332302f8e996c5ad6048bc6114118e1641a296e1
mahmoud-2egazy <mahmoud.tarboo@gmail.com>
2024-06-26 07:01:32 +0300

    Add files via upload

---
+++ plugins/بايدن.js
@@ -0,0 +1,12 @@
+let handler = async (m, { conn, args, text, usedPrefix, command }) => {
+  let too = `[❗] افصل النص يحب بـ *+*\n\n *مـثــال* :\n*${usedPrefix + command}* زورو معانا+زورو ضايع`
+
+  if (!text) throw too
+  let lr = (`https://api.popcat.xyz/biden?text=${encodeURIComponent(text)}`)
+  conn.sendFile(m.chat, lr, 'drake.png', `تم بواسطه 𝑻𝑨𝑹𝑩𝑶𝑶`, m)
+}
+handler.help = ['بايدن']
+handler.tags = ['maker']
+handler.command = ['بايدن','meme']
+
+export default handler
