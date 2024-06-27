ec2169fac1b5062c7a5f9366ee2b544affc41f4b
mahmoud-2egazy <mahmoud.tarboo@gmail.com>
2024-06-27 13:14:06 +0300

    Add files via upload

--- plugins/-قائمه-الاوامر.js
+++ plugins/-قائمه-الاوامر.js
@@ -1,1 +1,99 @@
+let handler = async (m, { conn, args,
+usedPrefix, command }) => {
+conn.relayMessage(m.chat, {
+viewOnceMessage: {
+message: {
+interactiveMessage: {
+header: {
+title: '> *﹝⟣┈┈┈⟢﹝🍄﹞⟣┈┈┈⟢﹞*\n> *مرحبا بك/يصديقي'
+ },
+ body: {
+ text: '> *﹝⟣┈┈┈⟢﹝🍄﹞⟣┈┈┈⟢﹞*\n> *✨هذة هيا قائمه الاوامر✨*\n> *قم بأختيار قسم من القائمه👇🏻*\n> *﹝⟣┈┈┈⟢﹝🍄﹞⟣┈┈┈⟢﹞*\n> *Copyright©* 2024 Mahmoud Al Tarboo.'
+  },
+  nativeFlowMessage: {
+  buttons: [
+   {
+  name: 'single_select',
+  buttonParamsJson: JSON.stringify({
+  title: '💫 MENU ALL 💫',
+  sections: [
+  {
+  title: 'قسم الاوامر',
+  highlight_label: '𝐁𝐎𝐓_𝐓𝐀𝐑𝐁𝐎𝐎',
+  rows: [
+  {
+  header: 'به جميع اوامر الرومات',
+  title: '👥Group👥',
+  description: '',
+  id: '.اوامر-الرومات'
+  },
+  {
+  header: 'هنا تجد اوامر التنزيلات 📥',
+  title: '📥Downloads📥✬',
+  description: '',
+  id: '.اوامر-التنزيلات'
+  },
+  {
+   header: 'هنا اوامر تغيير الصوت🔊',
+  title: '🔊Audios🔊',
+  description: '',
+  id: '.اوامر-الصوت'
+  },
+  {
+    header: 'كل الاوامر الخاصة بالتصميم ☢️',
+  title: '☢Design☢️',
+  description: '',
+  id: '.اوامر-التصميم'
+  },
+  {
+  header: 'كل الاوامر الاسلامية👳🏻‍♂ ',
+  title: '‍♂👳🏻‍Islamic👳🏻‍♂',
+  description: '',
+  id: '.اوامر-اسلامية'
+  },
+  {
+  header: 'هذا القسم خاص بالمطور فقط⚙🤴🏻',
+  title: '🤴🏻Owner🤴🏻',
+  description: '',
+  id: '.اوامر-المطور'
+  },
+   {
+    header: 'هاذا القسم للألقاب🖊️',
+  title: '✍️Nicknames✍️',
+  description: '',
+  id: '.اوامر-الألقاب'
+  },
+   {
+  header: 'هنا قـسم الالعاب والفعاليه🕹️',
+  title: '🎲Games🎲',
+  description: '',
+  id: '.اوامر-الالعاب'
+  }
+  ]
+  }
+  ]
+  }),
+  messageParamsJson: ''
+  }, 
+  {
+                                name: "cta_url",
+                                buttonParamsJson: JSON.stringify({
+                                    display_text: "مجموعه الدعم👥",
+                                    url: "https://chat.whatsapp.com/HTAcYFJ19sC07ykgnKqBzp",
+                                    merchant_url: "https://chat.whatsapp.com/HTAcYFJ19sC07ykgnKqBzp"
+  })
+  }
+  ]
+  }
+  }
+  }
+  }
+  }, {})
 
+  }
+
+  handler.help = ['info']
+  handler.tags = ['main']
+  handler.command = ['مهام','ty','er','youssef','ui','op']
+
+  export default handler
---
+++ plugins/_اوامر-التنزيلات.js
@@ -0,0 +1,82 @@
+import fetch from 'node-fetch';
+
+
+
+
+const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, isPrems}) => {
+  const datas = global
+  const idioma = datas.db.data.users[m.sender].language
+  const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`))
+  const tradutor = _translate.plugins.menu_audios
+
+  try {
+    const pp = imagen4;
+    // let vn = './media/menu.mp3'
+    const img = './Menu2.jpg';
+    const d = new Date(new Date + 3600000);
+    const locale = 'ar';
+    const week = d.toLocaleDateString(locale, {weekday: 'long'});
+    const date = d.toLocaleDateString(locale, {day: 'numeric', month: 'long', year: 'numeric'});
+    const _uptime = process.uptime() * 1000;
+    const uptime = clockString(_uptime);
+    const user = global.db.data.users[m.sender];
+    const {money, joincount} = global.db.data.users[m.sender];
+    const {exp, limit, level, role} = global.db.data.users[m.sender];
+    const rtotalreg = Object.values(global.db.data.users).filter((user) => user.registered == true).length;
+    const more = String.fromCharCode(8206);
+    const readMore = more.repeat(850);
+    const taguser = '@' + m.sender.split('@s.whatsapp.net')[0];
+    const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
+    const document = doc[Math.floor(Math.random() * doc.length)];
+    const str = `
+> *✧────[ 𝑾𝑬𝑳𝑪𝑶𝑴𝑬 ]────╮*
+> *┤ 🤴🏻 المطور :* *Mahmoud Mahmed*
+> *┤ #️⃣ الرقم :* *wa.me/201225655220*
+> *┤ ✅ الاصدار :* {1.2.0}
+> *┤ 🎳 البادئة :* *{•}*
+> *┤ *📆 التاريخ :*${date}*
+> *┤ 🚀 وقت التشغيل:* *${uptime}*
+> *┤────────────···*
+> *╮───[ اوامر التحميل ]───✧*
+> *┤ 〚 .تحميل-عام 〛*
+> *┤ 〚 .انستا 〛*
+> *┤ 〚 .فيس 〛*
+> *┤ 〚 .تويتر 〛*
+> *┤ 〚 .تيكتوك 〛*
+> *┤ 〚 .درايف 〛*
+> *┤ 〚 .سبوتيفاي 〛*
+> *┤ 〚 .ميديافاير 〛*
+> *┤ 〚 .يوتيوب-اغنيه 〛*
+> *┤ 〚 .يوتيوب-فيد 〛*
+> *┤ 〚 .يوتيوب-بحث 〛*
+> *┤ 〚 .يوتيوب-شغل 〛*
+> *┤ 〚 .جوجل-بلاي 〛*
+> *┤ 〚 .جوجل 〛*
+> *┤ 〚 .صوره 〛*
+> *┤ 〚 خلفيه 〛*
+> *┤ 〚 .درايف 〛*
+> *┤ 〚 .حزمه-استيكر 〛*
+> *╯────────────···*`.trim();
+    if (m.isGroup) {
+      // await conn.sendFile(m.chat, vn, 'menu.mp3', null, m, true, { type: 'audioMessage', ptt: true})
+      const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
+      conn.sendMessage(m.chat, {image: pp, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net')}, {quoted: fkontak2});
+    } else {
+      // await conn.sendFile(m.chat, vn, 'menu.mp3', null, m, true, { type: 'audioMessage', ptt: true})
+      const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
+      conn.sendMessage(m.chat, {image: pp, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net')}, {quoted: fkontak2});
+    }
+  } catch {
+    conn.reply(m.chat, tradutor.texto2, m);
+  }
+};
+handler.command = /^(قسم-التنزيلات|اوامر-التنزيلات|keyaudio|krk|jrn|jdj|bjjk|jdkn|memuaudio|audios|keyaudio|keyaudios)$/i;
+handler.exp = 50;
+handler.fail = null;
+export default handler;
+function clockString(ms) {
+  const h = isNaN(ms) ? '--' : Math.floor(ms / 3600000);
+  const m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60;
+  const s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60;
+  return [h, m, s].map((v) => v.toString().padStart(2, 0)).join(':');
+}
