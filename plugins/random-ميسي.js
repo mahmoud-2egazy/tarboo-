a52a40b16955ae5509748c36197c83f499009012
mahmoud-2egazy <mahmoud.tarboo@gmail.com>
2024-06-29 04:54:21 +0300

    Add files via upload

---
+++ plugins/random-ميسي.js
@@ -0,0 +1,10 @@
+import axios from 'axios'
+let handler = async(m, { conn, usedPrefix, command }) => {
+let res = (await axios.get(`https://raw.githubusercontent.com/BrunoSobrino/TheMystic-Bot-MD/master/src/JSON/Messi.json`)).data  
+let url = await res[Math.floor(res.length * Math.random())]
+conn.sendFile(m.chat, url, 'error.jpg', `*عمك ميسي 🇦🇷*`, m)}
+//conn.sendButton(m.chat, "*عمك معزة 🇦🇷*", author, url, [['𝙎𝙄𝙂𝙐𝙄𝙀𝙉𝙏𝙀 | 𝙉𝙀𝙓𝙏 🆕', `${usedPrefix + command}`]], m)}
+handler.help = ['messi']
+handler.tags = ['internet']
+handler.command = /^(messi|ميسي)$/i
+export default handler
