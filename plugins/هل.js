fd49ff969d3ad66a7929e06e46fd9b2e71bacd89
mahmoud-2egazy <mahmoud.tarboo@gmail.com>
2024-06-29 04:37:38 +0300

    Delete plugins directory

--- plugins/هل.js
+++
@@ -1,13 +0,0 @@
-let handler = async (m, { command, text }) => m.reply(`
-*💫 اسئل بوت تربو 💫*
-  
-*السؤال:* ${text}
-*الاجابة:* ${['اه','ممكن','في الاغلب اه','ف الاغلب لا','لا','مستحيل','حصل','مش عارف','نتا بتكلمني'].getRandom()}
-`.trim(), null, m.mentionedJid ? {
-mentions: m.mentionedJid
-} : {})
-handler.help = ['pregunta <texto>?']
-handler.tags = ['kerang']
-handler.command = /^هل$/i
-export default handler
-
