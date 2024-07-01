6a6e6a2171095ac0808045dca24dc844a42e9fe5
mahmoud-2egazy <mahmoud.tarboo@gmail.com>
2024-07-01 16:15:39 +0300

    Delete plugins directory

--- plugins/gp-تغيير-الينك.js
+++
@@ -1,8 +0,0 @@
-let handler = async(m, { conn }) => {
-let revoke = await conn.groupRevokeInvite(m.chat)
-await conn.reply(m.chat, `تم تغير رابط الجروب بنجاح 💫\n✨🫡 الرابط الجديد: ${'https://chat.whatsapp.com/' + revoke}`, m)}
-handler.command = ['revoke', 'رستر', 'تغيرالينك', 'لينك-جديد'] 
-handler.botAdmin = true
-handler.admin = true
-handler.group = true
-export default handler
