c564b9e44f3763d093f48d7931cfd959fe6e1d86
mahmoud-2egazy <mahmoud.tarboo@gmail.com>
2024-07-01 16:20:30 +0300

    Add files via upload

---
+++ plugins/_اوامر-الصوت.js
@@ -0,0 +1,79 @@
+import fetch from 'node-fetch';
+
+
+
+
+const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, isPrems}) => {
+  const datas = global
+  const idioma = datas.db.data.users[m.sender].language
+  const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`))
+  const tradutor = _translate.plugins.menu_audios
+
+  try {
+    const pp = imagen4;
+    // let vn = './media/menu.mp3'
+    const img = './Menu4.png';
+    const d = new Date(new Date + 3600000);
+    const locale = 'ar';
+    const week = d.toLocaleDateString(locale, {weekday: 'long'});
+    const date = d.toLocaleDateString(locale, {day: 'numeric', month: 'long', year: 'numeric'});
+    const _uptime = process.uptime() * 1000;
+    const uptime = clockString(_uptime);
+    const user = global.db.data.users[m.sender];
+    const {money, joincount} = global.db.data.users[m.sender];
+    const {exp, limit, level, role} = global.db.data.users[m.sender];
+    const rtotalreg = Object.values(global.db.data.users).filter((user) => user.registered == true).length;
+    const more = String.fromCharCode(8206);
+    const readMore = more.repeat(850);
+    const taguser = '@' + m.sender.split('@s.whatsapp.net')[0];
+    const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
+    const document = doc[Math.floor(Math.random() * doc.length)];
+    const str = `
+> *✧────[ 𝑾𝑬𝑳𝑪𝑶𝑴𝑬 ]────╮*
+> *┤ *مرحبا يا ${taguser}*
+> *┤ 🤴🏻 المطور :* *Mahmoud Mahmed*
+> *┤ #️⃣ الرقم :* *wa.me/201225655220*
+> *┤ ✅ الاصدار :* {1.2.0}
+> *┤ 🎳 البادئة :* *{•}*
+> *┤ 🧜🏽‍♂️ المستخدمين :* *${rtotalreg}* 
+> *┤ 🔃 المستوي :* *${level}* 
+> *┤  الخبرة :* *${exp}* 
+> *┤ *📆 التاريخ :*${date}*
+> *┤ 🚀 وقت النشاط:* *${uptime}*
+> *┤────────────···*
+> *╮───[ اوامر الصوت ]───✧*
+> *┤〚 .سنجاب 〛*
+> *┤〚 .سلس 〛*
+> *┤〚 .بطئ 〛*
+> *┤〚 .الي 〛*
+> *┤〚 .عكس 〛*
+> *┤〚 .تسريع-بسيط 〛*
+> *┤〚 .سريع 〛*
+> *┤〚 .تحسين 〛*
+> *┤〚 .منفوخ 〛*
+> *┤〚 .عميق 〛*
+> *┤〚 .عميق2 〛*
+> *╯────────────···*`.trim();
+    if (m.isGroup) {
+      // await conn.sendFile(m.chat, vn, 'menu.mp3', null, m, true, { type: 'audioMessage', ptt: true})
+      const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
+      conn.sendMessage(m.chat, {image: pp, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net')}, {quoted: fkontak2});
+    } else {
+      // await conn.sendFile(m.chat, vn, 'menu.mp3', null, m, true, { type: 'audioMessage', ptt: true})
+      const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
+      conn.sendMessage(m.chat, {image: pp, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net')}, {quoted: fkontak2});
+    }
+  } catch {
+    conn.reply(m.chat, tradutor.texto2, m);
+  }
+};
+handler.command = /^(اوامر-الصوت|قسم-الترفيه|keyaudio|krk|jrn|jdj|bjjk|jdkn|memuaudio|audios|keyaudio|keyaudios)$/i;
+handler.exp = 50;
+handler.fail = null;
+export default handler;
+function clockString(ms) {
+  const h = isNaN(ms) ? '--' : Math.floor(ms / 3600000);
+  const m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60;
+  const s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60;
+  return [h, m, s].map((v) => v.toString().padStart(2, 0)).join(':');
+}
---
+++ plugins/تفعيلات.js
@@ -0,0 +1,355 @@
+const handler = async (m, {conn, usedPrefix, command, args, isOwner, isAdmin, isROwner}) => {
+  const optionsFull = `*الخيار:* ✨ | الترحيب
+*الامر:* ${usedPrefix + command} الترحيب
+*الوصف:* فتح او قفل الترحيب في الجروب.
+
+--------------------------------
+
+*الخيار:* 🌎 | مود العام
+*الامر:* ${usedPrefix + command} عام
+*الوصف:* يصبح البوت علنا او خاصا.
+*الحاله:* لا يمكن استخدام هذا الامر الا بواسطه المطور.
+
+--------------------------------
+
+*الخيار:* 🔗 | مضاد اللينكات
+*الامر:* ${usedPrefix + command} مضاداللينكات
+*الحاله:* تشغيل او ايقاف مضاد لينكات الواتس.
+*الحاله:* يجب ان يكون مفعلا.
+
+--------------------------------
+
+*الخيار:* 🔗 | مضاد اللنكات 2
+*الامر:* ${usedPrefix + command} مضاداللينكات2
+*الوصف:* تشغيل وا اقاف مضاد اي لنكات.
+*الحاله:* يجب ان يكون مفعلا.
+
+--------------------------------
+
+*الخيار:* 🔎 | كشف
+*الامر:* ${usedPrefix + command} كشف
+*:* تنشيط او الغاء التعديل علي الوصف.
+
+--------------------------------
+
+*الخيار:* 🔎 | كشف 2
+*الامر:* ${usedPrefix + command} كشف2
+*الوصف:* يكشف التعديلات في المجموعه و يحافظ علي اداره افضل.
+
+--------------------------------
+
+*الخيار:* ❗ | يقيد
+*الامر:* ${usedPrefix + command} تقيد
+*وصف:* فتح او قفل قيود البوت مثلا  يطرد و يضيفه.
+*حاله:* المطور بس الي يستعمل الامر ده.
+
+--------------------------------
+
+*الخيار:* ☑️ | القرائه التلقائي
+*الامر:* ${usedPrefix + command} الصحين
+*الوصف:* فتح او قفل القرائه التلقائي.
+*الحاله:* المطور بس الي بيتسعمل الامر ده.
+
+--------------------------------
+
+*الخيار:* 🔊 |  اصوات
+*الامر:* ${usedPrefix + command} اصوات
+*الوصف:* فتح او قفل الريكات في الجروب.
+
+--------------------------------
+
+*الخيار:* 👾 | ستيكر تلقائي
+*الامر:* ${usedPrefix + command} ستيكرتلقائي 
+*الوصف:*تصبح جميع الصور أو مقاطع الفيديو المرسلة في المجموعة ملصقات. 
+
+--------------------------------
+
+*الخيار:* 💬 | خاص فقط
+*الامر:* ${usedPrefix + command} برايفت
+*الوصف:* سوف يستجيب البوت في الخاص بس.
+*الحاله:* المطور بس الي يقدر يستعمل الامر ده.
+
+--------------------------------
+
+*الخيار:* 🏢 | جروبات فقط
+*الامر:* ${usedPrefix + command} جروبات
+*الوصف:* البوت هيشتغل في الجروبات بس. 
+*الحاله:* المكور بس الي يستخدم الامر ده.
+
+--------------------------------
+
+*الخيار:* ❌ | مضاد الاخفاء
+*الامر:* ${usedPrefix + command} مضادالاخفاء
+*الوصف:* الصوره او الفيديو الذي يبعت ليرا مره واحده يبعت من البوت مره اخري بشكل طبيعي. 
+
+--------------------------------
+
+*الخيار:* 📵 | ممنوع الاتصال
+*الامر:* ${usedPrefix + command} مضادالاتصال
+*الوصف:* يبلك اي حد يرن علي رقم البوت. 
+*الحاله:* المطور بس الي يستخدم الامر ده.
+
+--------------------------------
+
+*الخاله:* 💬 | مضاد الخاص
+*الامر:* ${usedPrefix + command} مضادالخاص
+*الوصف:* يبلك اي حد يكلم البوت خاص. 
+*الحاله:* المطور بس الي يستخدم الامرد ده.
+
+--------------------------------
+
+*الخيار:* 🤬 | مضاد الشتائم
+*الامر:* ${usedPrefix + command} مضادالشتائم
+*الوصف:* يقوم بتحذير اي شخص سب او شتم او كتب شئ عيب واذا تجاوذ التحذيرات يقوم بطرده.
+*الحاله:* يجب ان يكون التقيد مفعلا.
+
+--------------------------------
+
+*الخيار:* 🤖 | البوت الفرعي
+*الامر:* ${usedPrefix + command} البوت-الفرعي
+*الحاله:* تفعيل و اقاف امر (${usedPrefix}serbot / ${usedPrefix}jadibot). 
+*الحاله:* المطور بس الي يقدر يستعمل الامر ده.
+
+--------------------------------
+
+*الخيار:* 👑 | الادمن
+*الامر:* ${usedPrefix + command} الادمن-فقط
+*الوصف:* سوف يجيب البوت علي الادمن فقط.
+
+--------------------------------
+
+*الخيار:* 😃 | سمسمي
+*الامر:* ${usedPrefix + command} سمسمي
+*الوصف:* هيبدا البوت يرد باستخدام الذكاء الصتناعي سمسمي.
+
+--------------------------------
+
+*الخيار:* ⏳ | مضاد الاسبام
+*الامر:* ${usedPrefix + command} مضادالاسبام
+*الوصف:* يكتشف البوت بعد ارسال 5 رسائل و يحظر المستخدم.
+*الخاله:* المطور بس الي يستخدم الامر ده.
+
+--------------------------------
+
+*الخيار:* 🛡️ | مضاد الحذف
+*الامر:* ${usedPrefix + command} مضادالحذف
+*الوصف:* يكتشف البوت الرساله المحذوفه و يقوم بتحويلها للمستخدم.
+
+--------------------------------
+
+*الخيار:* 🔊 | صوت_بوت
+*الامر:* ${usedPrefix + command} اصوات_البوت
+*الوصف:* يتم الغاء جميعرالصوات الخاصه بالبوت .
+*الحاله:* المطور بس الي يستخدم الامر ده.`.trim();
+
+  const isEnable = /true|افتح|enable|(turn)?on|1/i.test(command);
+  const chat = global.db.data.chats[m.chat];
+  const user = global.db.data.users[m.sender];
+  const bot = global.db.data.settings[conn.user.jid] || {};
+  const type = (args[0] || '').toLowerCase();
+  let isAll = false; const isUser = false;
+  switch (type) {
+    case 'الترحيب':
+      if (!m.isGroup) {
+        if (!isOwner) {
+          global.dfail('group', m, conn);
+          throw false;
+        }
+      } else if (!(isAdmin || isOwner || isROwner)) {
+        global.dfail('admin', m, conn);
+        throw false;
+      }
+      chat.welcome = isEnable;
+      break;
+    case 'كشف':
+      if (!m.isGroup) {
+        if (!isOwner) {
+          global.dfail('group', m, conn);
+          throw false;
+        }
+      } else if (!isAdmin) {
+        global.dfail('admin', m, conn);
+        throw false;
+      }
+      chat.detect = isEnable;
+      break;
+    case 'كشف2':
+      if (!m.isGroup) {
+        if (!isOwner) {
+          global.dfail('group', m, conn);
+          throw false;
+        }
+      } else if (!isAdmin) {
+        global.dfail('admin', m, conn);
+        throw false;
+      }
+      chat.detect2 = isEnable;
+      break;
+    case 'سمسمي':
+      if (m.isGroup) {
+        if (!(isAdmin || isOwner)) {
+          global.dfail('admin', m, conn);
+          throw false;
+        }
+      }
+      chat.simi = isEnable;
+      break;
+    case 'antiporno':
+      if (m.isGroup) {
+        if (!(isAdmin || isOwner)) {
+          global.dfail('admin', m, conn);
+          throw false;
+        }
+      }
+      chat.antiporno = isEnable;
+      break;
+    case 'delete':
+      if (m.isGroup) {
+        if (!(isAdmin || isOwner)) {
+          global.dfail('admin', m, conn);
+          throw false;
+        }
+      }
+      chat.delete = isEnable;
+      break;
+    case 'مضادالحذف':
+      if (m.isGroup) {
+        if (!(isAdmin || isOwner)) {
+          global.dfail('admin', m, conn);
+          throw false;
+        }
+      }
+      chat.antidelete = isEnable;
+      break;
+    case 'عام':
+      isAll = true;
+      if (!isROwner) {
+        global.dfail('rowner', m, conn);
+        throw false;
+      }
+      global.opts['self'] = !isEnable;
+      break;
+    case 'مضادالينكات':
+      if (m.isGroup) {
+        if (!(isAdmin || isOwner)) {
+          global.dfail('admin', m, conn);
+          throw false;
+        }
+      }
+      chat.antiLink = isEnable;
+      break;
+    case 'مضاداللينكات2':
+      if (m.isGroup) {
+        if (!(isAdmin || isOwner)) {
+          global.dfail('admin', m, conn);
+          throw false;
+        }
+      }
+      chat.antiLink2 = isEnable;
+      break;
+    case 'مضادالاخفاء':
+      if (m.isGroup) {
+        if (!(isAdmin || isOwner)) {
+          global.dfail('admin', m, conn);
+          throw false;
+        }
+      }
+      chat.antiviewonce = isEnable;
+      break;
+    case 'modohorny':
+      if (m.isGroup) {
+        if (!(isAdmin || isOwner)) {
+          global.dfail('admin', m, conn);
+          throw false;
+        }
+      }
+      chat.modohorny = isEnable;
+      break;
+    case 'الادمن-فقط':
+      if (m.isGroup) {
+        if (!(isAdmin || isOwner)) {
+          global.dfail('admin', m, conn);
+          throw false;
+        }
+      }
+      chat.modoadmin = isEnable;
+      break;
+    case 'ستيكرتلقائي':
+      if (m.isGroup) {
+        if (!(isAdmin || isOwner)) {
+          global.dfail('admin', m, conn);
+          throw false;
+        }
+      }
+      chat.autosticker = isEnable;
+      break;
+    case 'اصوات':
+      if (m.isGroup) {
+        if (!(isAdmin || isOwner)) {
+          global.dfail('admin', m, conn);
+          throw false;
+        }
+      }
+      chat.audios = isEnable;
+      break;
+    case 'تقيد':
+      isAll = true;
+      if (!isOwner) {
+        global.dfail('owner', m, conn);
+        throw false;
+      }
+      bot.restrict = isEnable;
+      break;
+    case 'اصوات_البوت':
+      isAll = true;
+      if (!isOwner) {
+        global.dfail('owner', m, conn);
+        throw false;
+      }
+      bot.audios_bot = isEnable;      
+      break;
+    case 'nyimak':
+      isAll = true;
+      if (!isROwner) {
+        global.dfail('rowner', m, conn);
+        throw false;
+      }
+      global.opts['nyimak'] = isEnable;
+      break;
+    case 'الصحين':
+      isAll = true;
+      if (!isROwner) {
+        global.dfail('rowner', m, conn);
+        throw false;
+      }
+      bot.autoread2 = isEnable;
+      global.opts['autoread'] = isEnable;
+      break;
+    case 'برايفت':
+    case 'privateonly':
+      isAll = true;
+      if (!isROwner) {
+        global.dfail('rowner', m, conn);
+        throw false;
+      }
+      global.opts['pconly'] = isEnable;
+      break;
+    case 'جروبات':
+    case 'grouponly':
+      isAll = true;
+      if (!isROwner) {
+        global.dfail('rowner', m, conn);
+        throw false;
+      }
+      global.opts['gconly'] = isEnable;
+      break;
+    case 'swonly':
+    ca
+      if (!isROwner) {
+        global.dfail('rowner'
+      global.opts['swonly'] = isEnable;
+      break;
+    case 'مضادالمكالمات':
+      isAll = true;
+      if (!isROwner) {
+        global.dfai
